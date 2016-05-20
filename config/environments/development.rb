a020da97a458d024f71050b1caab840539325d6e
silvio-galli <silvio.galli@gmail.com>
2016-05-21 00:18:17 +0200

    Added user sign in/out functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,15 @@ Rails.application.configure do
 
   # Action Mailer default host
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    domain:         ENV['GMAIL_DOMAIN'],
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+    enable_starttls_auto: true
+}
 end
