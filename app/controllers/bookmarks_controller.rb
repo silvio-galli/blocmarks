af85cfb4d58765294a3b293e200ffd3bc7db328c
silvio-galli <silvio.galli@gmail.com>
2016-05-23 11:12:54 +0200

    Added seed for topics and users

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_user!
+  
   def new
     @topic = Topic.find(params[:topic_id])
     @bookmark = Bookmark.new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+me = User.create!(
+  user_name:          "Fake",
+  email:              "fake@example.com",
+  password:           "password"
+)
+
+member = User.create!(
+  user_name:          "Member User",
+  email:              "member@example.com",
+  password:           "password"
+)
+
+5.times do
+  user = User.create!(
+    user_name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8)
+  )
+end
+
+users = User.all
+
+20.times do
+  topic = Topic.create!(
+    title: Faker::Hipster.word,
+    user: users.sample
+  )
+end
+
+10.times do
+  topic = Topic.create!(
+    title: Faker::Hipster.word,
+    user_id: "1"
+  )
+end
+
+10.times do
+  topic = Topic.create!(
+    title: Faker::Hipster.word,
+    user_id: "2"
+  )
+end
+
+puts "Seed finished!"
+puts "Blocmarks was populated with #{User.count} new users."
+puts "Blocmarks was populated with #{Topic.count} new topics."
