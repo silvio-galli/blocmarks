3b3a32d854d462a691fa8cf3467f259e448777fc
silvio-galli <silvio.galli@gmail.com>
2016-05-29 00:39:53 +0200

    Refactored incoming#create

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,37 +5,36 @@ class IncomingController < ApplicationController
 
   def create
     # Find the user by using params[:sender]
-    user = User.find(params[:sender])
-    # Find the topic by using params[:subject]
-    topic = Topic.find(params[:subject])
-    # Assign the url to a variable after retreiving it from params["body-plain"]
-    url = params["body-plain"].strip
-
-    # Check if user is nil, if so, create and save a new user
-    if user == nil
+    user = User.find_by(email: params[:sender])
+    # Check if user is nil
+    if user == nil || user == []
       # some actions to take if user is nil
       # e.i. send back an email
-
-    # Check if the topic is nil, if so, create and save a new topic
-    elsif
-      bookmark = Bookmark.new
-      bookmark.url = url
-      bookmark.user = user
-      # i topic == nil
-      # assign topic = "no topic"
-      if topic == nil
-        topic = Topic.where(title: "no topic")
-        bookmark.topic_id = topic.id
-      else
-        bookmark.topic = topic
-      end
-
-      # Now that you're sure you have a valid user and topic, build and save a new bookmark
-      bookmark.save
-
-      # Assuming all went well.
-      head 200
     end
+
+    # Find the topic by using params[:subject]
+    if params[:subject] == ""                         # if params[:subject] is empty
+      topic = Topic.where(title: "no topic")          # topic is assigned to "no topic"
+    else                                              # if params[:subject] is NOT empty, use it as topic
+      topic = Topic.find_by(title: params[:subject])  # assign topic to params[:subject]
+      if topic == nil || topic == []                  # if params[:subject] is not a topic already existent, it returns nil or []
+        topic = Topic.create(                         # so create a new topic based on params[:subject]
+          title: params[:subject],
+          user_id: user.id
+          )
+      end
+    end
+
+    url = params["body-plain"]                        # Assign the url to a variable after retreiving it from params["body-plain"]
+
+    bookmark = topic.bookmarks.build(                 # Now that you're sure you have a valid user and topic, build and save a new bookmark
+      url: url,
+      user_id: user.id
+    )
+    bookmark.save
+
+    # Assuming all went well.
+    head 200
   end
 
 end
