fb7ff508166b97d1cd4ab99433a17c6b1fd4f958
silvio-galli <silvio.galli@gmail.com>
2016-05-31 19:12:37 +0200

    Added like model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ActiveRecord::Base
-  belongs_to :topic
+  belongs_to :bookmark
   belongs_to :user
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :topics
   has_many :bookmarks
+  has_many :likes
 end
