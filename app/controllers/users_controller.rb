12deadc592746bf151a5975ff8d49e3dcf7af839
silvio-galli <silvio.galli@gmail.com>
2016-06-09 12:59:04 +0200

    merged user-profile-page branch

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+    @bookmarked_topics = @user.bookmarks.includes(:topic).group_by(&:topic)
+    @liked_bookmarks = @user.liked_bookmarks.includes(:topic).group_by(&:topic)
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def retrieve_like_from_bookmark(bookmark)
+    current_user.likes.where(bookmark_id: bookmark).take
+  end
 end
