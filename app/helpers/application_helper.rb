1c573f5153f693daa8ddc9c0d38bfe7e9904bd9a
silvio-galli <silvio.galli@gmail.com>
2016-06-10 11:00:30 +0200

    Partially refactored users#show view after loosing user-profile-page work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def retrieve_like_from_bookmark(bookmark)
-    current_user.likes.where(bookmark_id: bookmark).take
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :topics
   has_many :bookmarks
   has_many :likes
+  has_many :liked_bookmarks, through: :likes, source: :bookmark
 
   def liked?(bookmark)
     bookmark.likes.where(user_id: id).any?
